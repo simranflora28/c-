0e459c2861aa9b9199add1a477f64b99248f97b7
SIMRAN FLORA <57839965+simranflora28@users.noreply.github.com>
2020-11-25 13:50:08 -0500

    Create name.cpp

---
+++ name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+// request name
+	std::cout << "please enter your name:"; // read the name
+	std::string name; // define the name
+	std::cin >> name; // read the name
+	std::cout << name; // output the name
+	return 0;
+}
